76a909cde45bdfd6cae48451e6a6415ec16ded9c
Yohan Ardiansyah <yohan.ardiansyah90@gmail.com>
2020-08-12 11:10:32 +0700

    revisi class user

--- lib/src/classes/user.dart
+++ lib/src/classes/user.dart
@@ -1,7 +1,7 @@
 class User {
-  String nama_lengkap, nik, alamat;
+  String nama_lengkap, nik, alamat, email, password;
   DateTime tanggal_lahir;
   int money;
 
-  User(this.nama_lengkap, this.nik, this.tanggal_lahir, this.alamat, this.money);
+  User(this.nama_lengkap, this.nik, this.tanggal_lahir, this.alamat, this.money, this.email, this.password);
 }
