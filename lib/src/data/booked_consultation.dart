64d27923855059d8596300218c6deb2b764d85e3
Yohan Ardiansyah <yohan.ardiansyah90@gmail.com>
2020-08-16 19:08:02 +0700

    booked consultation time finished (can be improved)

---
+++ lib/src/data/booked_consultation.dart
@@ -0,0 +1,8 @@
+import 'package:menta/src/classes/offline_consultation.dart';
+import 'package:menta/src/data/dummy.dart';
+
+List<OfflineConsultation> list_booked_consultation = [
+  OfflineConsultation(start: DateTime.parse("1969-07-20 08:00:00Z"), finish: DateTime.parse("1969-07-20 10:00:00Z"), patient: list_user[2]),
+  OfflineConsultation(start: DateTime.parse("1969-07-20 10:30:00Z"), finish: DateTime.parse("1969-07-20 12:30:00Z"), patient: list_user[3]),
+  OfflineConsultation(start: DateTime.parse("1969-07-20 13:00:00Z"), finish: DateTime.parse("1969-07-20 15:00:00Z"), patient: list_user[4]),
+];
